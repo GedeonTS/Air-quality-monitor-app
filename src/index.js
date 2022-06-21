4c4ae2ae89e14a1a5e8a55c5b040536cda861dc4
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-21 19:02:57 +0200

    set up redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import store from './components/redux/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 
