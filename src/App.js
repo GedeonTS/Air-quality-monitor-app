65eda0f688f66c627d12617309e9d75807962a0c
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-21 10:56:15 +0200

    install react router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       {
-        axios.get('http://api.openweathermap.org/data/2.5/air_pollution?lat=48.8566&lon=2.3522&appid=f1d8a4d595c7a03c5224b9f44ae09d6d').then(res=>console.log(res.data))
+        axios.get('http://api.openweathermap.org/data/2.5/air_pollution?lat=48.6&lon=2.3&appid=f1d8a4d595c7a03c5224b9f44ae09d6d').then(res=>console.log(res.data))
       }
     </div>
   )
