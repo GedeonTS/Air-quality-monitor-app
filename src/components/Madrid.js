4fe73151022af4fd516f851c772cc3a4cc4451e9
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-22 17:47:18 +0200

    create madrid and rome components

---
+++ src/components/Madrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Madrid() {
+  return (
+    <div>Madrid</div>
+  )
+}
+
+export default Madrid
---
+++ src/components/Rome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Rome() {
+  return (
+    <div>Rome</div>
+  )
+}
+
+export default Rome
