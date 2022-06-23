d59c4587df797c90281caac9c9abf1d94c97a551
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-23 10:44:28 +0200

    display data for Rome

--- src/components/Rome.js
+++ src/components/Rome.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Stack } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { fetchData } from './redux/actions';
 
 function Rome() {
+  const data = useSelector((state) => state.data);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
   return (
-    <div>Rome</div>
-  )
+    <Stack gap={0} className="main-container">
+      <div className=" secondary-layer control"><Link to="/" className="arrow"><IoIosArrowBack /></Link></div>
+      <div className=" h-15 first-layer" style={{ background: 'linear-gradient(#e66465, #9198e5)' }}>ROME</div>
+      <div className=" secondary-layer description">CITY&apos;S AIR COMPOSITION DATA</div>
+      <div className="  secondary-layer">
+        <div>co:</div>
+        <div>{data[4].co}</div>
+      </div>
+      <div className="  secondary-layer link-1">
+        <div>NH3:</div>
+        <div>{data[4].nh3}</div>
+      </div>
+      <div className="  secondary-layer">
+        <div>NO:</div>
+        <div>{data[4].no}</div>
+      </div>
+      <div className="  secondary-layer link-1">
+        <div>NO2:</div>
+        <div>{data[4].no2}</div>
+      </div>
+      <div className="  secondary-layer">
+        <div>O3:</div>
+        <div>{data[4].o3}</div>
+      </div>
+      <div className="  secondary-layer link-1">
+        <div>PM2_5:</div>
+        <div>{data[4].pm2_5}</div>
+      </div>
+      <div className="  secondary-layer">
+        <div>PM10:</div>
+        <div>{data[4].pm10}</div>
+      </div>
+      <div className="  secondary-layer link-1">
+        <div>SO2:</div>
+        <div>{data[4].so2}</div>
+      </div>
+    </Stack>
+  );
 }
 
-export default Rome
+export default Rome;
