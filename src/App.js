3b5643b0288fc699e70a418d98d058ee4239bb91
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-23 10:38:06 +0200

    remove un-used variables

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react';
 import {
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from './components/redux/actions';
 import Home from './components/Home';
 import Paris from './components/Paris';
-import './App.css'
+import Warsaw from './components/Warsaw';
+import Barcelona from './components/Barcelona';
+import Rome from './components/Rome';
+import Lisbon from './components/Lisbon';
+import Madrid from './components/Madrid';
+import London from './components/London';
+import Brussels from './components/Brussels';
 
-
-// {
-//   axios.get('http://api.openweathermap.org/data/2.5/air_pollution?lat=48.6&lon=2.3&appid=f1d8a4d595c7a03c5224b9f44ae09d6d').then(res=>console.log(res.data))
-// }
-
+import './App.css';
 
 function App() {
+  const coord = useSelector((state) => state.coord);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
   return (
     <Routes>
-      <Route path="/paris" element={<Paris/>}/>
-      <Route path="/" element={<Home/>}/>
-  </Routes>
-  )
+      <Route path="/paris" element={<Paris />} />
+      <Route path="/Warsaw" element={<Warsaw />} />
+      <Route path="/Barcelona" element={<Barcelona />} />
+      <Route path="/Rome" element={<Rome />} />
+      <Route path="/Lisbon" element={<Lisbon />} />
+      <Route path="/Madrid" element={<Madrid />} />
+      <Route path="/London" element={<London />} />
+      <Route path="/Brussels" element={<Brussels />} />
+      <Route path="/" element={<Home coord={coord} />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
