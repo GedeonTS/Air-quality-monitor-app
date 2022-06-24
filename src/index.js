b3bdecb3ad169ad231df1405f3db4bf6fa6b0a1c
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-24 11:44:33 +0200

    set redux to new path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import store from './components/redux/configStore';
+import store from './redux/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
