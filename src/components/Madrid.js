44344484e4f8e51e8ef6da420b424c85a430c542
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-24 11:40:08 +0200

    pass Madrids data to city props

--- src/components/Madrid.js
+++ src/components/Madrid.js
@@ -1,10 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Stack } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { IoIosArrowBack } from 'react-icons/io';
-import { Link } from 'react-router-dom';
-import { fetchData } from './redux/actions';
+import { fetchData } from '../redux/actions';
+import City from './modules/City';
 
 function Madrid() {
   const data = useSelector((state) => state.data);
@@ -13,43 +10,17 @@ function Madrid() {
     dispatch(fetchData());
   }, []);
   return (
-    <Stack gap={0} className="main-container">
-      <div className=" secondary-layer control"><Link to="/" className="arrow"><IoIosArrowBack /></Link></div>
-      <div className=" h-15 first-layer" style={{ background: 'linear-gradient(#e66465, #9198e5)' }}>MADRID</div>
-      <div className=" secondary-layer description">CITY&apos;S AIR COMPOSITION DATA</div>
-      <div className="  secondary-layer">
-        <div>co:</div>
-        <div>{data[3].co}</div>
-      </div>
-      <div className="  secondary-layer link-1 ">
-        <div>NH3:</div>
-        <div>{data[3].nh3}</div>
-      </div>
-      <div className="  secondary-layer">
-        <div>NO:</div>
-        <div>{data[3].no}</div>
-      </div>
-      <div className="  secondary-layer link-1">
-        <div>NO2:</div>
-        <div>{data[3].no2}</div>
-      </div>
-      <div className="  secondary-layer">
-        <div>O3:</div>
-        <div>{data[3].o3}</div>
-      </div>
-      <div className="  secondary-layer link-1">
-        <div>PM2_5:</div>
-        <div>{data[3].pm2_5}</div>
-      </div>
-      <div className="  secondary-layer">
-        <div>PM10:</div>
-        <div>{data[3].pm10}</div>
-      </div>
-      <div className="  secondary-layer link-1">
-        <div>SO2:</div>
-        <div>{data[3].so2}</div>
-      </div>
-    </Stack>
+    <City
+      title="MADRID"
+      CO={data[3].co}
+      NH3={data[3].nh3}
+      N0={data[3].no}
+      NO2={data[3].no2}
+      O3={data[3].o3}
+      PM2_5={data[3].pm2_5}
+      PM10={data[3].pm10}
+      SO2={data[3].so2}
+    />
   );
 }
 
