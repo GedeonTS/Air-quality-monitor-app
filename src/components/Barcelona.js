b0263af863264d96a6b1eba14a2e376ee6299492
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-02 00:07:55 +0200

    Add hider method to city as props

--- src/components/Barcelona.js
+++ src/components/Barcelona.js
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../redux/actions';
 import City from './modules/City';
 
-function Barcelona() {
+function Barcelona(props) {
+  const { hider } = props;
   const data = useSelector((state) => state.data);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -11,15 +12,17 @@ function Barcelona() {
   }, []);
   return (
     <City
+      hider={hider}
       title="BARCELONA"
-      CO={data[5].co}
-      NH3={data[5].nh3}
-      N0={data[5].no}
-      NO2={data[5].no2}
-      O3={data[5].o3}
-      PM2_5={data[5].pm2_5}
-      PM10={data[5].pm10}
-      SO2={data[5].so2}
+      image="barcelona"
+      CO={data[5] ? data[5].co : ''}
+      NH3={data[5] ? data[5].nh3 : ''}
+      N0={data[5] ? data[5].no : ''}
+      NO2={data[5] ? data[5].no2 : ''}
+      O3={data[5] ? data[5].o3 : ''}
+      PM2_5={data[5] ? data[5].pm2_5 : ''}
+      PM10={data[5] ? data[5].pm10 : ''}
+      SO2={data[5] ? data[5].so2 : ''}
     />
   );
 }
